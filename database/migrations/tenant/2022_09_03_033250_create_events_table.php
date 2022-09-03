02942a799b66a4ddbbc82881cdd96810cbe0c998
Maetad Sukarasud <maetads@rabbit.co.th>
2022-09-03 16:20:47 +0700

    feat: :card_file_box: event migration

---
+++ database/migrations/tenant/2022_09_03_033250_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('detail')->nullable();
+            $table->enum('status', ['initiate', 'pre-event', 'event', 'post-event', 'end', 'postpone', 'cancel'])->nullable();
+            $table->json('dates')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
