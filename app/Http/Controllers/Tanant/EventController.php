ba77b91c4f7f76c9fce039e212153cfcf227359c
Maetad Sukarasud <maetads@rabbit.co.th>
2022-09-18 17:22:33 +0700

    feat: :sparkles: create an event

--- app/Http/Controllers/Tanant/EventController.php
+++ app/Http/Controllers/Tanant/EventController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Tanant;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Tanant\Event\CreateRequest;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class EventController extends Controller
 {
@@ -17,5 +19,21 @@ class EventController extends Controller
     {
         $events = Event::all();
         return response()->json($events);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CreateRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateRequest $request)
+    {
+        $event = new Event;
+        $event->name = $request->input('name');
+        $event->status = $request->input('status');
+        $event->save();
+
+        return response()->json($event, Response::HTTP_CREATED);
     }
 }
--- tests/Feature/Http/Controllers/Tanant/EventControllerTest.php
+++ tests/Feature/Http/Controllers/Tanant/EventControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\Http\Controllers\Tanant;
 
+use App\Enums\EventStatus;
 use App\Http\Controllers\Tanant\EventController;
+use App\Http\Requests\Tanant\Event\CreateRequest;
 use App\Models\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
@@ -29,5 +32,29 @@ class EventControllerTest extends TestCase
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertEquals(JsonResponse::HTTP_OK, $response->status());
         $this->assertCount(2, json_decode($response->content(), true));
+    }
+
+    /**
+     * Test store
+     *
+     * @return void
+     */
+    public function test_store_should_return_created()
+    {
+        $request = CreateRequest::create(
+            '',
+            Request::METHOD_POST,
+            [
+                'name' => 'event-name',
+                'status' => EventStatus::INITIATE->value,
+            ],
+        );
+        $controller = new EventController;
+        $response = $controller->store($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(JsonResponse::HTTP_CREATED, $response->status());
+        $this->assertStringContainsString('event-name', $response->content());
+        $this->assertStringContainsString(EventStatus::INITIATE->value, $response->content());
     }
 }
