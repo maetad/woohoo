74ef25e26c04e20cd2b0caafddcfe743021157b7
Maetad Sukarasud <maetads@rabbit.co.th>
2022-10-01 14:48:46 +0700

    feat: login request

---
+++ tests/Unit/Http/Requests/LoginRequestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Http\Requests;
+
+use App\Http\Requests\LoginRequest;
+use PHPUnit\Framework\TestCase;
+
+class LoginRequestTest extends TestCase
+{
+    public function test_it_should_return_rules()
+    {
+        $request = new LoginRequest;
+        $this->assertEquals([
+            'email' => 'required',
+            'password' => 'required',
+            'remember' => 'boolean',
+        ], $request->rules());
+    }
+}
