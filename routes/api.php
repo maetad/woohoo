50f259f657e2c339e14fb4030b22063bd44639e8
Maetad Sukarasud <maetads@rabbit.co.th>
2022-10-01 13:07:05 +0700

    refactor: :recycle: refactor api.php

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('', fn () => response()->json(['message' => 'welcome to woohoo']));
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
