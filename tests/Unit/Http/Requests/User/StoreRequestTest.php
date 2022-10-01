f9f05f8306984b2d6abcfc702e0e0b2b15e23c72
Maetad Sukarasud <maetads@rabbit.co.th>
2022-10-01 13:03:19 +0700

    feat: user requests

---
+++ tests/Unit/Http/Requests/User/StoreRequestTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Http\Requests\User;
+
+use App\Models\User;
+use App\Http\Requests\User\StoreRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rules\Unique;
+use PHPUnit\Framework\TestCase;
+
+class StoreRequestTest extends TestCase
+{
+    public function test_it_should_return_rules()
+    {
+        $request = new StoreRequest;
+        $this->assertEquals([
+            'name' => 'required',
+            'email' => [
+                'required',
+                new Unique(User::class, 'email'),
+            ],
+            'password' => [
+                'required',
+                Password::min(8)
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols(),
+            ]
+        ], $request->rules());
+    }
+}
