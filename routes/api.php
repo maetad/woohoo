13b1d2802d4f71b5ad97085a109e4db5d67da392
Maetad Sukarasud <maetads@rabbit.co.th>
2022-10-01 15:02:26 +0700

    feat: authentication controller

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', fn () => response()->json(['message' => 'welcome to woohoo']));
+Route::post('auth/login', 'AuthenticationController@login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
